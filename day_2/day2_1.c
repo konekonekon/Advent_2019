2148b79483ba1af24f326617266c58849e324db9
konekonekon <ayanotarumi@gmail.com>
2020-02-08 19:05:49 -0500

    Separate functions.

--- day_2/day2_1.c
+++ day_2/day2_1.c
@@ -2,86 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-int * calculate(int index, int * array);
 
-int * calculate(int index, int * array){
-	int operator = array[index];	
-	printf("Operator: %d\n", operator);
-	int l_oper_index = array[index+1];
-	int r_oper_index = array[index+2];
-	int output_index = array[index+3];
-
-	int l_number = array[l_oper_index];
-	int r_number = array[r_oper_index];
-
-	int result = 0;
-	if(operator==1){
-		result = l_number + r_number;
-		printf("%d + %d = %d\n", l_number, r_number, result);
-	} else if(operator==2){
-		result = l_number * r_number;
-		printf("%d * %d = %d\n", l_number, r_number, result);
-	} else if(operator==99){
-		printf("Finish!\n");
-		return array;
-	} else{
-		printf("Something wrong...\n");
-		int * empty = {0};
-		//int empty[];
-		//empty = {0};
-		return empty;
-	}
-
-	array[output_index] = result;
-	return array;
-}
-
-int main(){
-	//int result;
-	int length = 121;
-	int inputs[length];
-
-	FILE *fp = fopen("input", "r");
-	char *line_buf = NULL;
-	size_t line_buf_size = 0;
-	ssize_t line_size;
-	
-	//Read file
-	line_size = getline(&line_buf, &line_buf_size, fp);
-	while(line_size >= 0){
-		printf("contents: %s\n", line_buf);
-		
-		//split line
-		char * token = strtok(line_buf, ",");
-
-		int i = 0;
-		while(token != NULL){
-			inputs[i] = atoi(token);
-			//printf("content: %s\n", token);
-			//read next item
-			//NULL means the place we stopped before
-			token = strtok(NULL, ",");
-			i++;
-		}
-		printf("*Line length: %d\n", i);
-		line_size = getline(&line_buf, &line_buf_size, fp);
-	}	
-	free(line_buf);
-	line_buf = NULL;
-	fclose(fp);
-	
-	// print array
-	printf("Array: ");
-	for (int i=0; i<length; i++){
-		printf("%d,", inputs[i]);
-		if((i+1)%4==0){
-			printf(" / ");
-		}
-	}
-	printf("\n\n");
-
+void calculate(int length, int * inputs){
 	int index = 0;
-	
 	while(index < length){
 		int result;	
 		int operator = inputs[index];
@@ -107,8 +30,6 @@ int main(){
 			break;
 		}
 		inputs[output_index] = result;
-
-		//inputs = calculate(index, inputs);
 		index += 4;
 	
 		printf("New array: ");
@@ -119,8 +40,58 @@ int main(){
 			}
 		}
 		printf("\n\n");
-	}	
+	}		
+}
+
+
+void read_file(int * inputs, int length){
+	FILE *fp = fopen("input", "r");
+	char *line_buf = NULL;
+	size_t line_buf_size = 0;
+	ssize_t line_size;
 	
+	line_size = getline(&line_buf, &line_buf_size, fp);
+	while(line_size >= 0){
+		//printf("contents: %s\n", line_buf);
+		
+		//split line
+		char * token = strtok(line_buf, ",");
+
+		int i = 0;
+		while(token != NULL){
+			inputs[i] = atoi(token);
+			//printf("content: %s\n", token);
+			//read next item
+			//NULL means the place we stopped before
+			token = strtok(NULL, ",");
+			i++;
+		}
+		//printf("*Line length: %d\n", i);
+		line_size = getline(&line_buf, &line_buf_size, fp);
+	}	
+	free(line_buf);
+	line_buf = NULL;
+	fclose(fp);
+}
+
+int main(){
+	//int result;
+	int length = 121;
+	int inputs[length];
+
+	read_file(inputs, length);
+
+	//printf("Array: ");
+	//for (int i=0; i<length; i++){
+	//	printf("%d,", inputs[i]);
+	//	if((i+1)%4==0){
+	//		printf(" / ");
+	//	}
+	//}
+	//printf("\n\n");
+	
+	calculate(length, inputs);
+
 	printf("Result: ");
 	for (int i=0; i<length; i++){
 		printf("%d,", inputs[i]);
@@ -128,5 +99,3 @@ int main(){
 	printf("\n");
 	return EXIT_SUCCESS;
 }
-		
-
